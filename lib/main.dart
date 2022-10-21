9efd804be27b8c01449dca6a22b23667f0b41420
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-10-21 16:40:21 +0500

    Flutter Compass App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
+
+import 'package:compass_app/packages.dart';
 
 void main() => runApp(CompassApp());
 
@@ -10,21 +14,94 @@ class CompassApp extends StatefulWidget {
 }
 
 class _CompassAppState extends State<CompassApp> {
+  bool _hasPermission = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPermissionStatus();
+  }
+
+  void _fetchPermissionStatus() {
+    Permission.locationWhenInUse.status.then((status) {
+      if (mounted) {
+        setState(() {
+          _hasPermission = (status == PermissionStatus.granted);
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Compass App',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Scaffold(
+        body: SafeArea(
+          child: Builder(
+            builder: (context) {
+              if (_hasPermission) {
+                return _buildCompass();
+              } else {
+                return _buildPermission();
+              }
+            },
+          ),
+        ),
+      ),
     );
   }
-}
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  Widget _buildCompass() {
+    return StreamBuilder<CompassEvent>(
+      stream: FlutterCompass.events,
+      builder: (context, snapshot) {
+        //* Error
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error reading heading: ${snapshot.error}'),
+          );
+        }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+        //* Waiting
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
+
+        double? direction = snapshot.data!.heading;
+
+        if (direction == null) {
+          return Center(
+            child: Text('Device does not have sensors'),
+          );
+        }
+        return Center(
+          child: Container(
+            padding: EdgeInsets.all(25),
+            child: Transform.rotate(
+              angle: direction * (math.pi / 180) * -1,
+              child: Image.asset(
+                'assets/compass.png',
+                color: Colors.black,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildPermission() {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Permission.locationWhenInUse.request().then(
+                (value) => _fetchPermissionStatus(),
+              );
+        },
+        child: Text('Location Permission'),
+      ),
+    );
   }
 }
---
+++ lib/packages.dart
@@ -0,0 +1,3 @@
+// Packages
+export 'package:permission_handler/permission_handler.dart';
+export 'package:flutter_compass/flutter_compass.dart';
